b1e2172daa967723731d0899fe451fb2c3fba5a8
slummock <slummock@gmail.com>
2018-01-18 16:01:16 +0000

    removed redundant tests, added test for each single symbol

--- test/roman-numeral-generator-tests.js
+++ test/roman-numeral-generator-tests.js
@@ -3,36 +3,19 @@ var assert = chai.assert
 
 var numeralGenerator = require('../src/roman-numeral-generator.js')
 
-describe('Conversion tests', () => {
-    it('should return "I" when 1 is input', () => {
-        var input = 1
-        var expected = 'I'
-
-        var result = numeralGenerator.generate(input)
-
-        assert.equal(result, expected)
-    })
-    it('should return "V" when 5 is input', () => {
-        var input = 5
-        var expected = 'V'
-
-        var result = numeralGenerator.generate(input)
-
-        assert.equal(result, expected)
-    })
-})
-
-describe('Test case conversion tests', () => {
+describe('Conversion of each individual symbol', () => {
     var testCases =
         [
             { input: 1, result: 'I' },
             { input: 5, result: 'V' },
             { input: 10, result: 'X' },
-            { input: 20, result: 'XX' }
+            { input: 50, result: 'L' },
+            { input: 100, result: 'C' },
+            { input: 500, result: 'D' },
+            { input: 1000, result: 'M' }
         ]
 
     testCases.forEach(function (testCase) {
-        console.log(testCase)
         it('should return ' + testCase.result + ' when ' + testCase.input + ' is input', () => {
             assert.equal(numeralGenerator.generate(testCase.input), testCase.result)
         })
