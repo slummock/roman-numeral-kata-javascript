0a4127f21b4a7d59025aaf412d2d57572a4a7670
slummock <slummock@gmail.com>
2018-01-18 15:41:45 +0000

    refactoring

--- src/roman-numeral-generator.js
+++ src/roman-numeral-generator.js
@@ -1,19 +1,22 @@
 exports.generate = function (number) {
     var romanNumeral = 'UNSUPPORTED'
     if (validateInput(number)) {
-        romanNumeral = numeralValues[number]
+        romanNumeral = convertNumberToNumeral(number)
     }
     return romanNumeral
 }
 
-var numeralValues = {
-    1: 'I',
-    5: 'V',
-    10: 'X',
-    50: 'L',
-    100: 'C',
-    500: 'D',
-    1000: 'M'
+function convertNumberToNumeral (number) {
+    var numeralValues = {
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M'
+    }
+    return numeralValues[number]
 }
 
 function validateInput (number) {
